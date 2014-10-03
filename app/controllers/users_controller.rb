46053eb14cca34a9eb878ef32b37d395ad10fe82
App Academy Student <student@appacademy.io>
2014-10-03 11:56:47 -0400

    Finish user auth

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,58 @@
+class UsersController < ApplicationController
+
+  before_filter only: [:edit, :show, :update, :destroy] do
+    set_user
+  end
+
+  def index
+    @users = User.all
+    render :index
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_url
+    else
+      flash.now[:errors] = @user.errors.full_messages
+      render :new
+    end
+  end
+
+  def new
+    @user = User.new
+    render :new
+  end
+
+  def edit
+    render :edit
+  end
+
+  def show
+    render :show
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to user_url(@user)
+    else
+      flash.now[:errors] = @user.errors.full_messages
+      render :edit
+    end
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to users_url
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,45 @@
+class User < ActiveRecord::Base
+
+  attr_reader :password
+
+  validates :email, :password_digest, :session_token, presence: true
+  validates :password, length: {minimum: 6, allow_nil: true}
+
+  after_initialize do
+    ensure_session_token
+  end
+
+  def self.find_by_credentials(email, password)
+    user = User.find_by_email(email)
+    return nil if user.nil?
+    user.is_password?(password) ? user : nil
+  end
+
+
+  def generate_session_token
+    SecureRandom.base64(16)
+  end
+
+  def reset_session_token!
+    self.session_token = generate_session_token
+    self.save!
+    self.session_token
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  private
+
+  def ensure_session_token
+    self.session_token ||= generate_session_token
+  end
+
+end
